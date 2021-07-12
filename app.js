394eccb9a1f02af691f82fd2b3fd172f6741cf6f
Ayan <1100zaid@gmail.com>
2021-07-12 22:14:47 +0530

    mm

---
+++ app.js
@@ -0,0 +1,7 @@
+// When the user clicks on <div>, open the popup
+function myFunction() {
+    console.log("click");
+    var popup = document.querySelector(".popup");
+    popup.classList.toggle("show");
+
+  }
